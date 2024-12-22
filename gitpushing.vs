258c8ec2373b97e41ac576145ee901f56ba94cf3
Pushpendra1001 <pushpendra9122@gmail.com>
2024-12-22 21:27:56 +0530

    add explore page

--- gitpushing.vs
+++
@@ -1,5 +0,0 @@
-git add .
-read msg
-git commit -m $msg
-git add .
-git push
---
+++ lib/screens/explore.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class explorepage extends StatelessWidget {
+  const explorepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
